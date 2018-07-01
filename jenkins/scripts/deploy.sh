6f321b049ef513454e1a89c2231bc3bc34e8a1af
Juan Antonio Rodicio López <juan.antonio_decai10@hotmail.com>
2018-07-01 13:50:43 +0200

    Delete deploy.sh

--- jenkins/scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-jps -v | grep $1 | awk \'{print $1}\' | xargs kill || true
-BUILD_ID=dontKillMe env SERVER.PORT=8081 nohup java -jar -Dspring.profiles.active=prod ./target/$1-$2.jar > /dev/null 2>&1 &
